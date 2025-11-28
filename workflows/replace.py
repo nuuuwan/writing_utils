961faff8c80f76c443ba7914fe92a6c5c43bca3b
nuuuwan <nuuuwan@gmail.com>
2025-11-28 08:40:54 +0530

    Workflow for replace

---
+++ workflows/replace.py
@@ -0,0 +1,10 @@
+import sys
+
+from writing_utils import BookDir
+
+if __name__ == "__main__":
+    book_dir = BookDir.from_args_or_environs()
+    find_text = sys.argv[1]
+    replace_text = sys.argv[2]
+    prev_number_and_title = None
+    book_dir.replace(find_text, replace_text)
